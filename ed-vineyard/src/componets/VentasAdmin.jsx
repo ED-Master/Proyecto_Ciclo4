f0232687a6ad8f21602b4d8c15af45a75420e83b
santy0430 <sandarriaga@hotmail.com>
2022-10-31 22:11:06 -0500

    actualice datos

--- ed-vineyard/src/componets/VentasAdmin.jsx
+++ ed-vineyard/src/componets/VentasAdmin.jsx
@@ -2,36 +2,48 @@ import React from "react";
 import { useState } from "react";
 import ventas from "../listVentas.json"
 import { TotalVentas } from "./TotalVentas";
+import "../styles/VentasAdmin.css"
 
 // A D M I N I S T R A D O R
 
 export function VentasAdmin(){
     return(
-        <div>
+        <div className="container-ventasadmin">
           <h4>VENTAS</h4>
-          <table>
-            <thead>
-              <tr>
-                <th>Fecha</th>
-                <th>idVenta</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ventas.map( v =>(
-                <tr>
-                  <td>{v.fecha}</td>
-                  <td>{v.idVenta}</td>
-                  <td>${v.valor}</td>
-                </tr>
-              ))}
+          <div className="tabla">
+            <div className="table-wrapper">
+              <table >
+                <thead>
+                  <tr>
+                    <th>Fecha</th>
+                    <th>idVenta</th>
+                    <th>Valor</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ventas.map( v =>(
+                    <tr>
+                      <td>{v.fecha}</td>
+                      <td>{v.idVenta}</td>
+                      <td>${v.valor}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <table>
+              <tbody>
                 <tr>
                   <td></td>
                   <td>TOTAL:</td>
                   <td>$<TotalVentas></TotalVentas></td>
                 </tr>
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
 
           
         </div>
